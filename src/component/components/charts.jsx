471e76b6598cbdb7bfdf7ab7083b3222ef3e64ae
Bobby-gh <koomsonrobertgh@gmail.com>
2023-11-25 09:34:43 +0000

    s

--- src/component/components/charts.jsx
+++ src/component/components/charts.jsx
@@ -109,7 +109,7 @@ export function ReviewedVsUnreviewed() {
   }, []);
 
   return (
-    <div className=" items-center flex flex-col px-8 pb-5">
+    <div className=" items-center flex flex-col px-6 pb-5">
       <h3 className="pb-3">
         <span style={{ color: "#cc23b3" }}>REVIEWED </span>Vs{" "}
         <span style={{ color: "#2394cc" }}>UNREVIEWED</span>
