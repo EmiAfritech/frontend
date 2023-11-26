f04cbbce25559da8895f4102147ca74cf7fe8799
Bobby-gh <koomsonrobertgh@gmail.com>
2023-11-26 22:52:56 +0000

    tryingbg

--- src/pages/report/dashboard.jsx
+++ src/pages/report/dashboard.jsx
@@ -21,12 +21,12 @@ export function ReportRiskDashboard() {
       </div>
       <div ref={ref}>
         <div className="flex flex-col h-screen">
-          <div className="flex flex-row pb-3 pt-8">
+          <div className="flex flex-row pt-4">
             <ReportRiskLevel />
             <ReportRiskCategory />
             <ReportRiskLocation />
           </div>
-          <div className="flex flex-row pb-3 pt-3">
+          <div className="flex flex-row pt-3">
             <ReportRiskStatus />
             <ReportRiskResponse />
             <ReportRiskOwner />
