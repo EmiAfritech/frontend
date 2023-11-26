6389bad3434d80be024026576323d2438404232f
Bobby-gh <koomsonrobertgh@gmail.com>
2023-11-26 01:13:03 +0000

    £

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -74,7 +74,7 @@ export function RiskData(params) {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
+    bgcolor: "#bbdefb",
     boxShadow: 24,
     p: 4,
     borderRadius: 1,
