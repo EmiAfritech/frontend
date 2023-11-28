2898e6b4840a56aa05b933a6e83847b10a93075a
Bobby-gh <koomsonrobertgh@gmail.com>
2023-11-28 02:44:39 +0000

    @

--- src/component/components/charts.jsx
+++ src/component/components/charts.jsx
@@ -866,7 +866,7 @@ export function Pyramidchat() {
               className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               id="departmentName"
               aria-describedby="departmentName"
-              value={deptmentName}
+              value={departmentName}
               autoComplete="off"
               onChange={(e) => setdeptmentName(e.target.value)}>
               <option value="All Departments">All Departments</option>
