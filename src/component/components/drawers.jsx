d37c357c7a386e7839f5345eed42cbdba0f2c373
Bobby-gh <koomsonrobertgh@gmail.com>
2023-11-29 00:47:43 +0000

    @

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -629,7 +629,7 @@ export function Riskforms() {
               </div>
             </div>
 
-            <div className="relative" data-te-input-wrapper-init>
+            <div className="relative mb-6" data-te-input-wrapper-init>
               <textarea
                 type="text"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
