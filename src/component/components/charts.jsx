a0a8c114e3a6b490638a6ea77f6414f62a4e5fa1
Bobby-gh <koomsonrobertgh@gmail.com>
2023-12-05 18:11:55 +0000

    @

--- src/component/components/charts.jsx
+++ src/component/components/charts.jsx
@@ -1260,7 +1260,7 @@ export function HeatMap() {
             style={{
               backgroundColor: cell.value ? cell.color : 'white',
               height: '110px',
-              width: '200px',
+              width: '170px',
               borderRadius: '4px',
               textAlign: 'center',
               lineHeight: '50px',
