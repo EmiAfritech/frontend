f53856f2fdc323a8bc05697621af2ddebcbba55a
Bobby-gh <koomsonrobertgh@gmail.com>
2023-12-17 19:56:55 +0000

    @

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { FaTrashAlt, FaSave } from "react-icons/fa";
 import "../comstyles/component.css";
+import axios from "../../api/axios";
 import { DELETEUSER_URL, EDITUSER_URL } from "../../api/routes";
 
 export function UserData(params) {
