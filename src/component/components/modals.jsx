9a773a7ac212a0a88fe4a5bf90641cb5ac0ff59f
Bobby-gh <koomsonrobertgh@gmail.com>
2023-12-22 13:58:28 +0000

    @

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -496,6 +496,7 @@ export function RiskData(params) {
                   />
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
+                <InputLabel id="demo-simple-select-label">Age</InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     label="Department Name"
