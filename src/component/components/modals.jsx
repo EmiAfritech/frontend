29276ed86a6de164488250c9c1b405686f17d406
Bobby-gh <koomsonrobertgh@gmail.com>
2023-12-23 18:45:56 +0000

    @

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -1559,7 +1559,6 @@ export function DepartmentData(params) {
   const [createdAt, setCreatedAt] = useState(params.row.createdAt);
   const [updatedAt, setUpdatedAt] = useState(params.row.updatedAt);
   const [deletedAssociatedRisks, setDeletedAssociatedRisks] = useState(false);
-  setDeletedAssociatedRisks(false);
   const [deptmentName, setdeptmentName] = useState([]);
   
   const id = params.row.id;
