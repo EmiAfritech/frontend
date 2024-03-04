42483810073e46aaf9dd0bdc6f3109237daf0369
Bobby-gh <koomsonrobertgh@gmail.com>
2024-03-04 19:12:35 +0000

    @

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -68,6 +68,15 @@ export function Login() {
     } finally {
       setLoading(false);
     }
+
+    const handleReset = async (e) => {
+      if (email === "") {
+        alert("Enter your email");
+      } else {
+        localStorage.setItem("email", email);
+        navigate("/resetpassword", { replace: true });
+      }
+    };
   };
 
   return (
