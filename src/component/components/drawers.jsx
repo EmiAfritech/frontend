3cc900b54eb7e3a5907a90d12203c09050504f9e
lex_xenda <57144323+atogebaniaalexander@users.noreply.github.com>
2024-03-20 22:56:29 +0000

    Update drawers.jsx

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -733,11 +733,11 @@ export function Riskforms ({ onFormSubmit, tableData }) {
                     onChange={(e) => setProbabityilLevel(e.target.value)}
                     required>
                     <option></option>
-                    <option value="1">Almost Impossible</option>
-                    <option value="2">Unlikely</option>
-                    <option value="3">Likely</option>
-                    <option value="4">Very Likely</option>
-                    <option value="5">Almost Certain</option>
+                    <option value="1">Almost Impossible (1)</option>
+                    <option value="2">Unlikely (2)</option>
+                    <option value="3">Likely (3)</option>
+                    <option value="4">Very Likely (4)</option>
+                    <option value="5">Almost Certain (5)</option>
                   </select>
                   <label
                     htmlFor="department-head"
@@ -1414,11 +1414,11 @@ useEffect(() => {
                   }
                   required>
                   <option></option>
-                  <option value="1">Almost Impossible</option>
-                  <option value="2">Unlikely</option>
-                  <option value="3">Likely</option>
-                  <option value="4">Very Likely</option>
-                  <option value="4">Almost Certain</option>
+                  <option value="1">Almost Impossible (1)</option>
+                  <option value="2">Unlikely (2)</option>
+                  <option value="3">Likely (3)</option>
+                  <option value="4">Very Likely (4)</option>
+                  <option value="4">Almost Certain (5)</option>
                 </select>
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   probability-level
