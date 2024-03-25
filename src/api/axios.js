05c50b7726146ff9dc41f7903897cf28c7fa97e5
Atogebania Alexander <aatogebania@gmail.com>
2024-03-25 10:52:45 +0000

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "http://localhost:8001/api",
+  baseURL: "https://bkdeployments-production-5aac.up.railway.app/api",
 });
