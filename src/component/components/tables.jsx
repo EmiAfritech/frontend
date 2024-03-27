c7d143140c97614c490b7506372653278d5830e7
Bobby-gh <koomsonrobertgh@gmail.com>
2024-03-27 08:36:56 +0000

    @

--- src/component/components/tables.jsx
+++ src/component/components/tables.jsx
@@ -938,7 +938,7 @@ export function RiskStatusReportTab() {
         </div>
       </div>
       <div
-        style={{ height: 650, width: 850, backgroundColor: "white" }}
+        style={{ height: 650, width: 750, backgroundColor: "white" }}
         className=" mt-2 w-auto card p-4">
         <DataGrid
           rows={tableData}
