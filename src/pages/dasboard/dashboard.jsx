6bee0102f47eb6b9f69a93e1899c72e3bd25f430
Bobby-gh <koomsonrobertgh@gmail.com>
2024-04-15 16:07:42 +0000

    @

--- src/pages/dasboard/dashboard.jsx
+++ src/pages/dasboard/dashboard.jsx
@@ -9,23 +9,18 @@ import {
 import "../../component/comstyles/component.css";
 
 export function Dashboard() {
-  
   return (
-    <div className="flex de-flex bg-slate-100">
-      <Sidebar/>
-      <div className="m-6 mt-16 flex de-flex mx-auto pl-[18%] ">
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-            <div className="flex flex-row">
-              <OpenVsClose />
-              <ReviewedVsUnreviewed />
-              <MitigatedVsUnmitigated />
-              <MonitoredVsUnmonitored />
-            </div>
-            <div className="flex flex-row">
-              <RiskLineChart />
-            </div>
-          </div>
+    <div className="flex flex-col bg-slate-100">
+      <Sidebar />
+      <div className="m-6 mt-16 pl-[18%] ">
+        <div className="grid grid-cols-4 gap-4">
+          <OpenVsClose />
+          <ReviewedVsUnreviewed />
+          <MitigatedVsUnmitigated />
+          <MonitoredVsUnmonitored />
+        </div>
+        <div>
+          <RiskLineChart />
         </div>
       </div>
     </div>
