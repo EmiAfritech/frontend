2f2afcd1d2560b4a61e0845179e8d18e0183ed24
Bobby-gh <koomsonrobertgh@gmail.com>
2024-04-19 18:14:24 +0000

    @

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1951,7 +1951,7 @@ export function RiskMonitoringforms({ onFormSubmit }) {
                 mitigation-owner
               </label>
             </div>
-            <div className="relative mb-6" data-te-input-wrapper-init>
+            <div className="relative mb-4" data-te-input-wrapper-init>
               <select
                 type="text"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
