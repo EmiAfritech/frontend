c7ac1ad825d220dd0a0995c15cbe7bddfd738509
Bobby-gh <koomsonrobertgh@gmail.com>
2024-04-19 17:19:33 +0000

    @

--- src/pages/report/report.jsx
+++ src/pages/report/report.jsx
@@ -5,7 +5,7 @@ import { ReportSideTabs } from "./sidetabs";
 export function Report() {
   
   return (
-    <div className="flex flex-col bg-slate-100 h-screen">
+    <div className="flex flex-col bg-slate-100 h-lvh">
       <Sidebar/>
       <div className="grid grid-cols-4 gap-2 ml-[18%] m-6">
         <div className="mx-auto mt-3">
