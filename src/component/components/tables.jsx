1149bd1b41e95e669dccea6d6b614ea82c875f65
Bobby-gh <koomsonrobertgh@gmail.com>
2024-04-20 13:33:15 +0000

    @

--- src/component/components/tables.jsx
+++ src/component/components/tables.jsx
@@ -579,7 +579,7 @@ export function RiskViewTable() {
                 paginationModel: { page: 0, pageSize: 10 },
               },
             }}
-            pageSizeOptions={[15. 20]}
+            pageSizeOptions={[15, 20]}
             getCellClassName={(params) => {
               if (params.value === "High") {
                 return "high";
