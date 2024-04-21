afba07e7ccf0ec9666503d3813c3bd44921ae19d
Bobby-gh <koomsonrobertgh@gmail.com>
2024-04-21 22:11:46 +0000

    @

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1173,6 +1173,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
   const [dept, setDept] = useState([]);
   const [ownersName, setOwnersName] = useState([]);
   const [departmentID, setdepartmentID] = useState(" ");
+  const [riskCategory, setRiskCategory] = useState(" ");
   const [endDate, setEndDate] = useState(new Date());
   const [mitigatedRiskProbabilityLevel, setmitigatedRiskProbabilityLevel] =
     useState("");
@@ -1247,8 +1248,8 @@ export function RiskMitigationforms({ onFormSubmit }) {
           }
         );
 
-        // setRiskIDs(response);
-        console.log(response)
+        setRiskIDs(response.data.riskName);
+        setRiskCategory(response.data.riskCategory)
       } catch (error) {
         console.error(error);
       }
@@ -1416,7 +1417,8 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 <></>
               )}
             </div>
-            <div className="relative mb-6" data-te-input-wrapper-init>
+            <div className="grid grid-cols-3 gap 2">
+            <div className="relative mb-6 cols-span-2" data-te-input-wrapper-init>
               <select
                 type="riskID"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
@@ -1435,9 +1437,24 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 ))}
               </select>
               <label className="before:content[' '] after:content[' ']  pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                risk-id
+                risk-name
               </label>
             </div>
+            <div className="relative mb-6" data-te-input-wrapper-init>
+              <input
+                type="text"
+                className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                
+                value={riskCategory}
+                disabled
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                category
+              </label>
+            </div>
+
+            </div>
+            
             <div className="grid grid-cols-2 gap-2">
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <select
