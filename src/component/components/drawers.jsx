833d2990a9d72a6f729db1e373ded48533949313
Atogebania Alexander <aatogebania@gmail.com>
2024-04-22 08:56:13 +0000

    fixed

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1445,13 +1445,13 @@ export function RiskMitigationforms({ onFormSubmit }) {
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
               <input
-                type="riskID"
+                type="riskid"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="riskID"
                 aria-describedby="riskID"
                 value={iD}
                 autoComplete="off"
-                disabled
+                
                 >
                 
               </input>
