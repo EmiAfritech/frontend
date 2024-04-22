97755659b1c9ea1bbe8a253cc0fc7ae710b4616d
Atogebania Alexander <aatogebania@gmail.com>
2024-04-22 10:03:31 +0000

    chanfet

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1317,7 +1317,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
         await axios.post(
           MITIGATERISKFORM_URL,
           JSON.stringify({
-            riskID,
+            iD,
             mitigatedRiskProbabilityLevel,
             mitigatedRiskImpactLevel,
             mitigationControl,
@@ -1339,7 +1339,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
         await axios.post(
           MITIGATERISKFORM_URL,
           JSON.stringify({
-            riskID,
+            iD,
             mitigatedRiskProbabilityLevel,
             mitigatedRiskImpactLevel,
             mitigationControl,
@@ -1448,7 +1448,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 required>
                 <option></option>
                 {risks.map((risks) => (
-                  <option key={risks.id} value={risks.id} >
+                  <option key={risks.id} value={risks.riskID} >
                     {" "}
                     {risks.riskName
                     }
@@ -1467,7 +1467,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="riskID"
                 aria-describedby="riskID"
-                value={iD}
+                value={riskID}
                 autoComplete="off"
                 />
                 
