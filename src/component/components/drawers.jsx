89eebe2c45187aeda5062ad75eb787f82b92fe41
Atogebania Alexander <aatogebania@gmail.com>
2024-04-23 01:01:39 +0000

    changes to review

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -901,6 +901,7 @@ export function RiskReviewforms({ onFormSubmit }) {
   const [NextRiskReviewDate, setNextRiskReviewDate] = useState("");
   const [riskReviewComments, setriskReviewComments] = useState("");
   const [isLoading, setLoading] = useState(false);
+  const canDisplay = useState(false);
 
   const notify = () => {
     toast.success("Review Saved Successfully", {
@@ -1129,11 +1130,14 @@ export function RiskReviewforms({ onFormSubmit }) {
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   risk-review
                 </label>
+                {riskReview === "reject risk" ? (
+                  canDisplay = true
+                ): canDisplay = false}
               </div>
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
              
-               { riskReview === "reject risk" ? (
+               { canDisplay === true ? (
                <> 
                 {setNextRiskReviewDate(new Date())}
                   <input
