f1dcb2f5195b495ea43d9a4a9f870e959ea95a29
Atogebania Alexander <aatogebania@gmail.com>
2024-04-24 16:03:12 +0000

    chanfe456f

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1223,7 +1223,7 @@ export function RiskReviewforms({ onFormSubmit }) {
 }
 export function RiskMitigationforms({ onFormSubmit }) {
   const [riskName, setRiskName] = useState("");
-  let iD = "";
+ 
   const [risks, setRiskIDs] = useState([]);
   const [dept, setDept] = useState([]);
   const [ownersName, setOwnersName] = useState([]);
@@ -1237,8 +1237,10 @@ export function RiskMitigationforms({ onFormSubmit }) {
   const [riskReviewer, setRiskReviewer] = useState("");
   const [mitigationCost, setmitigationCost] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [riskID, riskCategory, impactLevel, probabilityLevel] = riskName.split(',');
-  
+const [riskID, riskCategory, impactLevel, probabilityLevel] = riskName.split(',');
+
+const impactLevelNumber = getImpactLevelNumber(parseInt(impactLevel, 10));
+const probabilityLevelNumber = getProbabiltyLevelNumber(parseInt(probabilityLevel, 10));  
   const hostaddress = "http://localhost:5173/risk-mitigation";
 
   const notify = () => {
@@ -1536,7 +1538,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                   className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                   id="impact"
                   aria-describedby="riskID"
-                  value={impactLevel}
+                  value={impactLevelNumber}
                   autoComplete="off"
                 />
 
@@ -1550,7 +1552,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                   className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                   id="probability"
                   aria-describedby="riskID"
-                  value={probabilityLevel}
+                  value={probabilityLevelNumber}
                   autoComplete="off"
                 />
 
@@ -1574,7 +1576,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                   <option value="2">Unlikely (2)</option>
                   <option value="3">Likely (3)</option>
                   <option value="4">Very Likely (4)</option>
-                  <option value="4">Almost Certain (5)</option>
+                  <option value="5">Almost Certain (5)</option>
                 </select>
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   mitigated-probability
