4609523909e5ad8dae2dcbacac9e57f18ae5fd7f
lex_xenda <57144323+atogebaniaalexander@users.noreply.github.com>
2024-06-12 13:15:57 +0000

    trying the sidebar design on the report sidebar

--- src/pages/report/sidetabs.jsx
+++ src/pages/report/sidetabs.jsx
@@ -95,6 +95,50 @@ const ManagernAuditor = () => {
 }
 
 
+/**
+ * import "../../component/comstyles/component.css";
+import { Link } from "react-router-dom";
+
+export function ReportSideTabs() {
+  return (
+    <div className="report-container">
+      <div className="report-main mt-20 mx-10">
+        <ul>
+          <li>
+            <Link to="">Report</Link>
+          </li>
+          
+          <li>
+            <Link to="risk-appetite">Risk Appetite</Link>
+          </li>
+          <li>
+            <Link to="mitigation">Mitigations By Date</Link>
+          </li>
+          <li>
+            <Link to="risk-status-report">Risk Status</Link>
+          </li>
+          <li>
+            <Link to="review-needing-risks">
+              All Opened Risks Needing a Review
+            </Link>
+          </li>
+          <li>
+            <Link to="audit-trail">Audit Trail</Link>
+          </li>
+          <li>
+            <Link to="advice">Risk Advice</Link>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+*/
+
+
+
+
 
 
 
