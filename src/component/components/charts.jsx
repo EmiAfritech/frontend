dd967a008edd431c2f246eca37e7611b5843f294
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-17 18:37:58 +0000

    checking route data for risk line

--- src/component/components/charts.jsx
+++ src/component/components/charts.jsx
@@ -367,7 +367,7 @@ export function RiskLineChart() {
 
     fetchData();
   }, [year]);
-
+console.log(data)
   const handleYearChange = (e) => {
     setYear(e.target.value);
     console.log(year);
