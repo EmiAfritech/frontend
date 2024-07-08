6610c458232bbc240204befe7ede37c3bde9ab41
Bobby-gh <knaihv@ymail.com>
2024-07-08 14:13:40 +0000

    Update axios.js changed base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "https://bkdeployments-production-5aac.up.railway.app/api",
+  baseURL: "http://bkdeployments-production.up.railway.app/api",
 });
