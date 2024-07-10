6d02fc7ffd06a4d138c8955816cfaab2107a0d04
Bobby-gh <koomsonrobertgh@gmail.com>
2024-07-10 15:44:53 +0000

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "http://bkdeployments-production.up.railway.app/api",
+  baseURL: "https://bkdeployments-production.up.railway.app/api",
 });
