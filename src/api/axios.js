3e8fdafca857d181b648e6b24c1c317679b2e30b
Bobby-gh <koomsonrobertgh@gmail.com>
2024-07-10 14:29:04 +0000

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "http://bkdeployments-production.up.railway.app/api",
+  baseURL: "http://riskappbackend.eu-4.evennode.com/api",
 });
