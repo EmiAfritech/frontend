0067915ee10dd3d9898474601de3024598a3021b
Bobby-gh <koomsonrobertgh@gmail.com>
2024-07-29 10:48:30 +0000

    fixing side bar

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -813,7 +813,7 @@ export function Riskforms({ onFormSubmit, tableData }) {
               </div>
             </div>
             <div >
-              <div className="relative" data-te-input-wrapper-init>
+              <div className="relative pb-4" data-te-input-wrapper-init>
                 <select
                   type="text"
                   className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
