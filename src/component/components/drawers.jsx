1a26943409d118e04f3d942f41f4ed17f114ef80
Bobby-gh <koomsonrobertgh@gmail.com>
2024-07-29 10:32:58 +0000

    fixing side bar

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -1523,7 +1523,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 </label>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-2">
+            <div>
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="impact"
@@ -1555,7 +1555,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
                 </label>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-2">
+            <div >
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <select
                   className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
