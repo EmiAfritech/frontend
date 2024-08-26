83246b7c7a1f903c15cf25fe43c7ab79afc99e81
Bobby-gh <koomsonrobertgh@gmail.com>
2024-08-26 19:20:47 +0000

    working on sidebar algorithm

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -670,7 +670,7 @@ export function RiskData(params) {
                   />
                 </div>
                 <div className="relative mb-6" data-te-input-wrapper-init>
-                  {auth.role === "MANAGER" || "AUDITOR" ? (
+                  {auth.role === "MANAGER" || auth.role === "AUDITOR" ? (
                     <TextField
                       label="Department ID"
                       value={departmentID}
