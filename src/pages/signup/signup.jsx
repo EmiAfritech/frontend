64de58fd33099c87594724998e3aed51230d048a
Bobby-gh <koomsonrobertgh@gmail.com>
2024-08-28 02:11:52 +0000

    working on sidebar algorithm

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -9,77 +9,74 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    firstName: "",
+    lastName: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-  const notifyNetworkError = () => {
-    toast.error("Server is Currently Unavailable, Please Try Again Later", {});
-  };
-  const notifyUnauthorizedUser = () => {
-    toast.error("Unauthorized User! Please check your credentials", {});
-  };
-  const notifyReturningNull = () => {
-    toast.info("Authorization returned null", {});
-  };
+  const notifyError = (message) => toast.error(message);
+  const notifyInfo = (message) => toast.info(message);
 
-  const notifyFillForms = () => {
-    toast.error("Kindly check Input details");
-  };
-
-  const reload = () => {
-    setEmail("");
-    setPassword("");
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    setLoading(true);
+    setIsLoading(true);
 
     try {
       const response = await axios.post(
         LOGIN_URL,
-        JSON.stringify({ email, password }),
+        JSON.stringify(formData),
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       );
 
-      if (response.status === 200) {
-        const token = response.data.authToken;
-        const role = response.data.role;
-        const department = response.data.department;
+      const { status, data } = response;
 
-        if (token && role) {
-          localStorage.setItem("token", token);
-          localStorage.setItem("role", role);
-          localStorage.setItem("departmentID", department);
-          navigate("/dashboard", { replace: true });
-        } else {
-          notifyReturningNull();
-        }
+      if (status === 201) {
+        navigate("/", { replace: true });
+      } else {
+        notifyInfo("Authorization returned null");
       }
     } catch (err) {
       if (err.message.includes("Network Error")) {
-        notifyNetworkError();
-        reload();
-      } else if (err.response.status === 401) {
-        notifyUnauthorizedUser();
-      } else if (err.response.status === 400 || err.response.status === 404) {
-        notifyFillForms();
+        notifyError("Server is Currently Unavailable, Please Try Again Later");
+        resetForm();
+      } else if (err.response?.status === 401) {
+        notifyError("Unauthorized User! Please check your credentials");
+      } else if ([400, 404].includes(err.response?.status)) {
+        notifyError("Kindly check input details");
       }
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   };
-  const handleReset = async (e) => {
-    if (email === "") {
+
+  const resetForm = () => {
+    setFormData({
+      email: "",
+      password: "",
+      name: "",
+      firstName: "",
+      lastName: "",
+    });
+  };
+
+  const handleReset = () => {
+    if (!formData.email) {
       alert("Enter your email");
     } else {
-      localStorage.setItem("email", email);
+      localStorage.setItem("email", formData.email);
       navigate("/resetpassword", { replace: true });
     }
   };
@@ -87,154 +84,97 @@ export function SignUp() {
   return (
     <>
       <ToastContainer hideProgressBar />
-      <div className="flex flex-row flex-direction">
+      <div className="flex flex-row">
         <div className="basis-2/3 background"></div>
-        <div className="basis-1/3 ">
+        <div className="basis-1/3">
           <div className="login-container">
             <div className="formstyle flex-col">
               <img
                 src="https://afriquetek.com/wp-content/uploads/2023/07/afriquetek-logo-1.png"
-                alt="Paris"
+                alt="Afriquetek Logo"
                 className="w-55 h-20"
               />
-              <form>
-                {/* organization */}
+              <form onSubmit={handleSubmit}>
                 <div>
-                  <div>
-                    <label>Organizational Name</label>
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      id="email"
-                      value={email}
-                      autoComplete="off"
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                  </div>
+                  <label>Organizational Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-                {/* email */}
-                <div className=" grid grid-cols-2">
-                  <div>
+                <div className="grid grid-cols-2">
                   <div>
                     <label>First Name</label>
-                  </div>
                     <input
                       type="text"
-                      id="email"
-                      value={firstName}
-                      autoComplete="off"
-                      onChange={(e) => setFirstName(e.target.value)}
+                      id="firstName"
+                      value={formData.firstName}
+                      onChange={handleInputChange}
                       required
                     />
                   </div>
-                  <div>
                   <div>
                     <label>Last Name</label>
-                  </div>
                     <input
                       type="text"
-                      id="email"
-                      value={lastName}
-                      autoComplete="off"
-                      onChange={(e) => setLastName(e.target.value)}
+                      id="lastName"
+                      value={formData.lastName}
+                      onChange={handleInputChange}
                       required
-                    />
-                  </div>
-                </div>
-                {/* email */}
-                {/* <div className="">
-                  <div>
-                    <input
-                      type="text"
-                      id="email"
-                      value={email}
-                      autoComplete="off"
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                  </div>
-                </div> */}
-                {/*employees in the organization*/}
-                {/* <div>
-                  <div>
-                    <label htmlFor="email">Number of Employees</label>
-                  </div>
-                  <input type="number" />
-                </div> */}
-                {/* username */}
-                <div className="">
-                  <div>
-                    <label htmlFor="email">Email</label>
-                  </div>
-                  <div>
-                    <input
-                      type="email"
-                      id="email"
-                      value={email}
-                      autoComplete="off"
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                  </div>
-                </div>
-                {/* password */}
-                <div>
-                  <div>
-                    <label htmlFor="password">Password</label>
-                  </div>
-                  <div>
-                    <input
-                      type="password"
-                      id="password"
-                      autoComplete="off"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                 </div>
                 <div>
-                  <div>
-                    <label htmlFor="password">Confirm Organizational Password</label>
-                  </div>
-                  <div>
-                    <input
-                      type="password"
-                      id="password"
-                      autoComplete="off"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-                {/* login-btn */}
+                <div>
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Confirm Password</label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
                 <button
                   className="login hover:bg-[#2a36b8]"
                   type="submit"
-                  onClick={handleSubmit}
-                  disabled={isLoading} // Disable the button while loading
+                  disabled={isLoading}
                 >
                   {isLoading ? (
-                    <div className="flex flex-row justify-center">
-                      <p className="text-sm pr-2">Loading</p>
-                      <CircularProgress
-                        size={27}
-                        thickness={6}
-                        color="primary"
-                      />
+                    <div className="flex justify-center">
+                      <span className="text-sm pr-2">Loading</span>
+                      <CircularProgress size={27} thickness={6} />
                     </div>
                   ) : (
                     "Submit"
                   )}
                 </button>
-                {/* create a new account */}
                 <div className="new-user">
-                  <span style={{ color: "blue" }}>
-                    <Link className="new" to="/">
-                    Have you already registered? Return to Login
-                    </Link>
-                  </span>
+                  <Link to="/" className="new" style={{ color: "blue" }}>
+                    Already registered? Return to Login
+                  </Link>
                 </div>
               </form>
             </div>
