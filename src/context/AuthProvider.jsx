b71c5fe834c63d88e7a99c134726b3c76947cfa3
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-05 23:00:06 +0000

    modifying sessions

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -16,10 +16,38 @@ export const AuthProvider = ({ children }) => {
       token: "",
       role: "",
       departmentID: "",
-      au
     });
   };
 
+  if (token === "") {
+   alert("token empty")
+  }
+
+  useEffect(() => {
+    try {
+      axios
+      .post(Sessions_URL, JSON.stringify({ token }), {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((data) => {
+        setSession(data.data.message);
+        if (session === "Invalid") {
+          alert("session invalid")
+        } 
+        
+      })
+      .catch((err) => {
+        if (err.message.includes("Network Error")) {
+          notifyNetwork()
+        }    
+      });
+    } catch (error) {
+     if (error.message.includes("Network Error")) {
+      notifySystem()
+      } 
+    }
+    
+  });
 
   return (
     <AuthContext.Provider value={{ auth, setAuth, clearAuth }}>
