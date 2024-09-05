f907320e8dc346097ac2841013cd8cd77d99e1dc
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-05 12:12:44 +0000

    working on risk status

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -668,16 +668,13 @@ export function Riskforms({ onFormSubmit, tableData }) {
             auth.role === "GENERALMANAGER" ? (
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <select
-                  type="departmentID"
                   className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                  id="departmentID"
-                  aria-describedby="departmentID"
+                  id="departmentName"
                   value={departmentName}
                   autoComplete="off"
                   onChange={(e) => setDepartmentName(e.target.value)}
                   required>
-                  <option> hi</option>
-
+                  <option></option>
                   {deptmentName.map((deptmentName) => (
                     <option
                       key={deptmentName.names.id}
