01d6f103f2e1e1b26e75ddecdabe9c664d74fcbc
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-06 08:32:59 +0000

    working on risk status

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -19,35 +19,7 @@ export const AuthProvider = ({ children }) => {
     });
   };
 
-  if (auth?.token === "") {
-   alert("token empty")
-  }
 
-  useEffect(() => {
-    try {
-      axios
-      .post(Sessions_URL, JSON.stringify({ token }), {
-        headers: { "Content-Type": "application/json" },
-      })
-      .then((data) => {
-        setSession(data.data.message);
-        if (session === "Invalid") {
-          alert("session invalid")
-        } 
-        
-      })
-      .catch((err) => {
-        if (err.message.includes("Network Error")) {
-          notifyNetwork()
-        }    
-      });
-    } catch (error) {
-     if (error.message.includes("Network Error")) {
-      notifySystem()
-      } 
-    }
-    
-  });
 
   return (
     <AuthContext.Provider value={{ auth, setAuth, clearAuth }}>
