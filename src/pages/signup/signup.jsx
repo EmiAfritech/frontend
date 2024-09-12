16e307a107500d7ff7999dca8edd22ce2fd23ac0
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-12 18:15:07 +0000

    working on activation page

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -57,7 +57,7 @@ export function SignUp() {
 
       if (status === 201) {
         alert("Organization Created Successfully, Kindly Check your Mail");
-        navigate("/activation", { replace: true });
+        navigate("/activate", { replace: true });
       } else {
         notifyInfo("Authorization returned null");
       }
