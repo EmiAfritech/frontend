34b5ff4ee38ea9389f46da54948870e13a1b99d2
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-12 13:03:53 +0000

    updated route

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,12 @@ import { ActivationPage } from "./pages/ActivateAccount/activatePage";
 function App() {
   return (
     <BrowserRouter>
-      <Sessions/>
+      <Sessions />
       <Routes>
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path = "/activate" element = {<ActivationPage/>}/>
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Routes>
+        <Route path="/activate" element={<ActivationPage />} />
       </Routes>
       <Routes path="/" element={<Sidebar />}>
         <Route path="/" element={<Login />} />
