c126f5e02f37bfa7cbcb03887db3c801d59abd19
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-12 00:58:51 +0000

    updated route

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "https://bkdeployments-production.up.railway.app/api",
+  baseURL: "https://risksaasbackend-production.up.railway.app/api",
 });
