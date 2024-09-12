4de8096660ba06089b09565cd13610aa233952c6
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-12 17:19:32 +0000

    working on activation page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -180,7 +180,7 @@ export function Login() {
                 <div className="new-user">
                   <span>{t("registerQuestion")}</span>{" "}
                   <span style={{ color: "blue" }}>
-                    <Link className="new" to="/activate">
+                    <Link className="new" to="/signup">
                       {t("register")}
                     </Link>
                   </span>
