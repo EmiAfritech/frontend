bbadada5ec25c70a8a9f084b0ea0fd0ac27a89a6
Bobby-gh <koomsonrobertgh@gmail.com>
2024-09-13 13:45:13 +0000

    working on password page

--- src/pages/createPassword/createPassword.jsx
+++ src/pages/createPassword/createPassword.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; // Import CSS for toastify
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import afriquetek_logo from "../../assets/images/afriquetek_logo.png";
+import "../login/login.css";
 
 export function CreatePasswordPage() {
   const location = useLocation();
@@ -17,9 +17,8 @@ export function CreatePasswordPage() {
   const [isActivating, setIsActivating] = useState(false);
   const [activationSuccess, setActivationSuccess] = useState(false);
 
-  // Get the token and email from the passed location state
   useEffect(() => {
-    if (location.state?.email && location.state?.token) {
+    if (location.state?.email) {
       setEmail(location.state.email);
       setFormData((prevData) => ({
         ...prevData,
@@ -78,7 +77,7 @@ export function CreatePasswordPage() {
     <>
       <ToastContainer hideProgressBar />
       <div className="flex flex-row h-screen">
-        <div className="basis-2/3 bg-gray-200"></div>
+        <div className="basis-2/3 background"></div>
         <div className="basis-1/3 flex items-center justify-center">
           <div className="login-container">
             <div className="flex-col m-16 flex items-center">
@@ -88,13 +87,13 @@ export function CreatePasswordPage() {
                 className="w-52 h-auto mb-12"
               />
               <form className="w-full" onSubmit={handleActivation}>
-                <div className="activation-container w-full">
+                <div className=" w-full">
                   {activationSuccess ? (
                     <p>Your account has been activated. You can now log in.</p>
                   ) : (
                     <div className="w-full">
                       <p className="mb-4 p-4 border border-blue-300 rounded-full font-bold text-sm w-full">
-                        Email: {email}
+                        email: {email}
                       </p>
                       <div className="w-full mb-4">
                         <label className="block mb-1 text-xs" htmlFor="password">
