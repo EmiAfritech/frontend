9030a75872b85869578cbb9bb00fe9a7338b0215
Bobby-gh <knaihv@ymail.com>
2024-10-22 11:48:17 +0000

    adding margin to login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -144,7 +144,7 @@ export function Login() {
                   className="w-36 h-20 mb-12 "
                 />
               </div>
-              <div className="mx-16 ">
+              <div className="mx-8 ">
                 <form onSubmit={handleSubmit}>
                   {/* Email */}
                   <div className="mb-5">
