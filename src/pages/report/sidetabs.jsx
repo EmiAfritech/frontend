99e925adcd5eb877fc84e883b3a17f289cd8a8dc
Bobby-gh <knaihv@ymail.com>
2025-02-02 01:48:56 +0000

    working on create password page

--- src/pages/report/sidetabs.jsx
+++ src/pages/report/sidetabs.jsx
@@ -140,10 +140,23 @@ export function ReportSideTabs() {
 
 
 
-
-"use client";
-import React, {useState} from "react";
-import { Tabs } from "./tabs";
+export const Tabs = [
+  {
+    title: "Balance Scorecard",
+  },
+  {
+    title: "Operational Effeciency",
+  },
+  {
+    title: "Strategic Performance",
+  },
+  {
+    title: "System Goals",
+  },
+  {
+    title: "Badges",
+  },
+];
 
 
 export default function ReportSideTabs({ onTabChange }) {
