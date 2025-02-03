5deb9018a4b59be45e2db527cf3b49b2c71eabf9
Bobby-gh <knaihv@ymail.com>
2025-02-03 12:01:02 +0000

    working on translations

--- src/api/sessions.jsx
+++ src/api/sessions.jsx
@@ -16,7 +16,7 @@ export function Sessions() {
   const navigate = useNavigate();
   const location = useLocation();
   const token = auth?.token;
-  console.log(token)
+  console.log({"sessions": token})
 
   const notifyUnauthorized = () => {
     toast.error("Unauthorized User!", {
