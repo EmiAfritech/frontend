caa76dc3f4970e067de49d08c9996f3e059b35ef
Bobby-gh <knaihv@ymail.com>
2025-02-03 22:38:43 +0000

    changing repo

--- src/api/sessions.jsx
+++ src/api/sessions.jsx
@@ -15,7 +15,7 @@ export function Sessions() {
   const { clearAuth, auth } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
-  const token = Cookies.get("token")
+  const token = auth?.token
   console.log({"sessions": token})
 
   const notifyUnauthorized = () => {
