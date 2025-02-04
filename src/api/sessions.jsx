85907a8497806bd1ff9f450d007c31aebc6f2084
Bobby-gh <knaihv@ymail.com>
2025-02-04 12:52:40 +0000

    changing repo

--- src/api/sessions.jsx
+++ src/api/sessions.jsx
@@ -106,9 +106,9 @@ const LoadingPopup = () => {
   }, []);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
-    </div>
+    <Backdrop open={isLoading} sx={{zIndex: 9999,color: "#fff",backgroundColor: "rgba(0, 0, 0, 0.3)",}}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 };
 
