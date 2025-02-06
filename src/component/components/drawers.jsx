24b8169b5e8d5020abbc5cb7aa8ac512ed4ea35b
Bobby-gh <knaihv@ymail.com>
2025-02-06 22:53:02 +0000

    working on side bar

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -60,16 +60,15 @@ function getImpactLevelNumber(impact) {
 export function Userforms({ onFormSubmit }) {
   const { auth } = useContext(AuthContext);
   const { t } = useTranslation();
-  const [userName, setUserName] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [departmentName, setDepartmentName] = useState("");
-  const [deptmentNames, setdeptmentNames] = useState([]);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [role, setRole] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setLoading] = useState(false);
+  const {departmentList} = useDepartmentDropdown()
 
   const notify = () => {
     toast.success("User Saved Successfully", {
@@ -87,22 +86,7 @@ export function Userforms({ onFormSubmit }) {
     toast.error("Server is currently down Contact your admin");
   };
 
-  useEffect(() => {
-    axios
-      .get(DEPARTMENTDROPDOWN_URL, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + auth.token,
-        },
-        withCredentials: true,
-      })
-      .then((data) => {
-        setdeptmentNames(data.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -112,7 +96,6 @@ export function Userforms({ onFormSubmit }) {
       await axios.post(
         USERSCREATEFORM_URL,
         JSON.stringify({
-          userName,
           firstName,
           lastName,
           departmentName,
@@ -174,20 +157,6 @@ export function Userforms({ onFormSubmit }) {
         <hr />
         <form className="w-96">
           <div className=" px-10 py-8">
-            <div className="relative mb-6" data-te-input-wrapper-init>
-              <input
-                type="text"
-                className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                id="Username"
-                value={userName}
-                autoComplete="off"
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-              <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                {t("username")}
-              </label>
-            </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="text"
@@ -258,12 +227,12 @@ export function Userforms({ onFormSubmit }) {
                     required>
                     <option></option>
 
-                    {deptmentNames.map((deptmentNames) => (
+                    {departmentList.map((departmentList) => (
                       <option
-                        key={deptmentNames.names.id}
-                        value={deptmentNames.names.name}>
+                        key={departmentList.names.id}
+                        value={departmentList.names.name}>
                         {" "}
-                        {deptmentNames.names.name}
+                        {departmentList.names.name}
                       </option>
                     ))}
                   </select>
@@ -502,7 +471,6 @@ export function Riskforms({ onFormSubmit, tableData }) {
   const [isLoading, setLoading] = useState(false);
   const [open, setOpen] = React.useState(false);
   const {departmentList} = useDepartmentDropdown()
-  const [deptmentName, setdeptmentName] = useState([]);
   useEffect(() => {
     setTableDataTest(tableData.map((item) => item.riskID));
   }, [tableData]);
