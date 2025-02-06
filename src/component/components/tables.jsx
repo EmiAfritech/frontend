f21e7325742b5eacbd2772551e7f66d5caa639d7
Bobby-gh <knaihv@ymail.com>
2025-02-06 23:50:43 +0000

    working on side bar

--- src/component/components/tables.jsx
+++ src/component/components/tables.jsx
@@ -541,7 +541,10 @@ export function DepartmentTab() {
       elevation: 0,
       sx: {
         borderRadius: "10",
-        zIndex: "1"
+        
+      },
+      style: {
+        zIndex: "1",
       },
     },
     muiTableBodyProps: {
