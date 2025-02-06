f7d295c241c1a698e78939865fdab677adc3ec60
Bobby-gh <knaihv@ymail.com>
2025-02-06 13:57:59 +0000

    workging on modals

--- src/component/components/modals.jsx
+++ src/component/components/modals.jsx
@@ -2347,7 +2347,7 @@ export function RiskAdviceReportData() {
       </IconButton>
       {open && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-          <div className="relative bg-white p-6 rounded-lg shadow-lg w-[40vw] h-[60vh]">
+          <div className="relative bg-white p-6 rounded-lg shadow-lg w-[80vw] h-[95vh]">
             {/* Close button in top-right corner */}
             <button
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 mb-6"
