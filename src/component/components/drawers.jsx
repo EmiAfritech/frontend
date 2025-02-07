af2310a4b24eaeeb83f89ae6828a5a0453103155
Bobby-gh <knaihv@ymail.com>
2025-02-07 10:38:34 +0000

    workging on modals

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -25,7 +25,7 @@ import {
 } from "../../api/routes";
 import { useTranslation } from "react-i18next";
 import { useDepartmentDropdown } from "../../api/routes-data";
-import { CustomButton, CDSFormInputField } from "./widgets";
+import { CustomButton, FormInputField } from "./widgets";
 
 function getProbabiltyLevelNumber(probabilitys) {
   if (probabilitys === 1) {
@@ -61,16 +61,21 @@ function getImpactLevelNumber(impact) {
 export function Userforms({ onFormSubmit }) {
   const { auth } = useContext(AuthContext);
   const { t } = useTranslation();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [departmentName, setDepartmentName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [role, setRole] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setLoading] = useState(false);
   const { departmentList } = useDepartmentDropdown();
   const [open, setOpen] = React.useState(false);
+  const [userValue, setUserValue] = [{
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+  }]
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setUserValue((prevData) => ({ ...prevData, [id]: value }));
+  };
 
   function handleOpen() {
     setOpen(!open);
@@ -165,9 +170,9 @@ export function Userforms({ onFormSubmit }) {
                 type="text"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="firstName"
-                value={firstName}
+                value={userValue.firstName}
                 autoComplete="off"
-                onChange={(e) => setFirstName(e.target.value)}
+                onChange={handleInputChange}
                 required
               />
               <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -179,9 +184,9 @@ export function Userforms({ onFormSubmit }) {
                 type="text"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="lastName"
-                value={lastName}
+                value={userValue.lastName}
                 autoComplete="off"
-                onChange={(e) => setLastName(e.target.value)}
+                onChange={handleInputChange}
                 required
               />
               <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -193,9 +198,9 @@ export function Userforms({ onFormSubmit }) {
                 type="email"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="Email"
-                value={email}
+                value={userValue.email}
                 autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleInputChange}
                 required
               />
               <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -207,9 +212,9 @@ export function Userforms({ onFormSubmit }) {
                 type="phoneNumber"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="phoneNumber"
-                value={phoneNumber}
+                value={userValue.phoneNumber}
                 autoComplete="off"
-                onChange={(e) => setPhoneNumber(e.target.value)}
+                onChange={handleInputChange}
                 required
               />
               <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -219,14 +224,14 @@ export function Userforms({ onFormSubmit }) {
             <div className="relative mb-6" data-te-input-wrapper-init>
               {auth.role === "ADMIN" || auth.role === "GENERALMANAGER" ? (
                 <>
-                  <select
+                  {/* <select
                     type="departmentID"
                     className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     id="departmentID"
                     aria-describedby="departmentID"
-                    value={departmentName}
-                    autoComplete="off"
-                    onChange={(e) => setDepartmentName(e.target.value)}
+                    // value={departmentName}
+                    // autoComplete="off"
+                    // onChange={(e) => setDepartmentName(e.target.value)}
                     required>
                     <option></option>
 
@@ -241,7 +246,7 @@ export function Userforms({ onFormSubmit }) {
                   </select>
                   <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                     {t("department")}
-                  </label>
+                  </label> */}
                 </>
               ) : (
                 <></>
@@ -252,9 +257,9 @@ export function Userforms({ onFormSubmit }) {
                 type="password"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="password"
-                value={password}
+                value={userValue.password}
                 autoComplete="off"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handleInputChange}
                 required
               />
               <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
@@ -262,7 +267,7 @@ export function Userforms({ onFormSubmit }) {
               </label>
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
-              <select
+              {/* <select
                 type="text"
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 id="role"
@@ -273,10 +278,10 @@ export function Userforms({ onFormSubmit }) {
                 <option></option>
                 <option value="MANAGER">{t("departmentManager")}</option>
                 <option value="AUDITOR">{t("riskAnalyst")}</option>
-              </select>
-              <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              </select> */}
+              {/* <label className="text-blue-800 pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 {t("role")}
-              </label>
+              </label> */}
             </div>
           </div>
 
@@ -304,7 +309,7 @@ export function Userforms({ onFormSubmit }) {
 }
 
 export function Departmentforms({ onFormSubmit }) {
-  const { auth } = React.useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
   const { t } = useTranslation();
   const [name, setName] = useState("");
   const [deptID, setDeptID] = useState("");
@@ -365,7 +370,7 @@ export function Departmentforms({ onFormSubmit }) {
     setLocation("");
   };
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   function handleOpen() {
     setOpen(!open);
@@ -392,7 +397,7 @@ export function Departmentforms({ onFormSubmit }) {
         <form className="w-96">
           <div className=" px-10 py-10">
             <div className="relative mb-6" data-te-input-wrapper-init>
-              <CDSFormInputField
+              <FormInputField
                 id="departmentID"
                 label={t("departmentCode")}
                 value={deptID}
--- src/component/components/widgets.jsx
+++ src/component/components/widgets.jsx
@@ -34,111 +34,160 @@ export function InputField({
         />
       </div>
     );
-  }
+}
   
-  export function CustomButton({
-    label = "Click me",
-    onClick,
-    type = "button",
+export function CustomButton({
+label = "Click me",
+onClick,
+type = "button",
+disabled = false,
+loading = false,
+className = "",
+}) {
+return (
+    <button
+    type={type}
+    onClick={onClick}
+    disabled={disabled || loading}
+    className={`w-full p-2 bg-[#1E4D7E] rounded-xl text-white rounded-lg ${className} ${
+        disabled || loading ? "opacity-50 cursor-not-allowed" : ""
+    }`}>
+    {loading ? (
+        <span className="flex items-center justify-center">
+        {/* Spinner */}
+        <svg
+            className="animate-spin h-5 w-5 mr-2 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24">
+            <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+            />
+            <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V4a10 10 0 00-10 10h2zm2 5.292A7.964 7.964 0 014 12H2a10 10 0 0016.292 7.292l-1.414-1.414A8.003 8.003 0 016 17.292z"
+            />
+        </svg>
+        Loading...
+        </span>
+    ) : (
+        label
+    )}
+    </button>
+);
+}
+
+export const CustomSelect = ({
+isMulti = false,
+fieldLabel = false,
+value,
+onChange,
+error,
+required = false,
+id,
+label,
+options = [],
+group = true,
+}) => {
+return (
+    <div>
+    {label && (
+        <label
+        htmlFor={id}
+        className={`block text-[12.5px] text-[#08376B] ${
+            error ? "text-red-500" : ""
+        }`}>
+        {label} {required && <span className="required">*</span>}
+        </label>
+    )}
+    {fieldLabel && <div className="my-2.5" />}
+    <Select
+        id={id}
+        isMulti={isMulti}
+        required={required}
+        isSearchable={true}
+        options={options}
+        onChange={(selectedOption) =>
+        group
+            ? onChange(id, selectedOption ? selectedOption.value : "")
+            : onChange(selectedOption.value)
+        }
+        defaultValue={
+        group ? options.find((option) => option.value === value) : value
+        }
+        isClearable={true}
+        styles={{
+        control: (baseStyles, state) => ({
+            ...baseStyles,
+            height: "40px",
+            padding: "2px",
+            backgroundColor: "#E5E7EB",
+            border: "none",
+            borderRadius: "0",
+        }),
+        }}>
+        <option>select ...</option>
+        {options.map((option) => (
+        <option key={option.value} value={option.value}>
+            {option.label}
+        </option>
+        ))}
+    </Select>
+    </div>
+);
+};
+
+export function FormInputField({
+    fieldLabel = false,
+    label,
     disabled = false,
-    loading = false,
-    className = "",
+    type = "text",
+    value,
+    placeholder,
+    onChange,
+    required = false,
+    error = "",
+    id,
+    IsMultiInput = false,
   }) {
     return (
-      <button
-        type={type}
-        onClick={onClick}
-        disabled={disabled || loading}
-        className={`w-full p-2 bg-[#1E4D7E] rounded-xl text-white rounded-lg ${className} ${
-          disabled || loading ? "opacity-50 cursor-not-allowed" : ""
-        }`}>
-        {loading ? (
-          <span className="flex items-center justify-center">
-            {/* Spinner */}
-            <svg
-              className="animate-spin h-5 w-5 mr-2 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24">
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              />
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V4a10 10 0 00-10 10h2zm2 5.292A7.964 7.964 0 014 12H2a10 10 0 0016.292 7.292l-1.414-1.414A8.003 8.003 0 016 17.292z"
-              />
-            </svg>
-            Loading...
-          </span>
-        ) : (
-          label
-        )}
-      </button>
-    );
-  }
-
-  export const CustomSelect = ({
-    isMulti = false,
-    fieldLabel = false,
-    value,
-    onChange,
-    error,
-    required = false,
-    id,
-    label,
-    options = [],
-    group = true,
-  }) => {
-    return (
-      <div>
-        {label && (
-          <label
-            htmlFor={id}
-            className={`block text-[12.5px] text-[#08376B] ${
-              error ? "text-red-500" : ""
-            }`}>
-            {label} {required && <span className="required">*</span>}
-          </label>
-        )}
+      <div className="mb-1">
+        <div className="flex flex-row">
+          {label && (
+            <label
+              className={`block text-[12.5px] text-[#08376B] ${
+                (error ? "text-red-500" : "", disabled ? "text-gray-300" : "")
+              }`}>
+              {label} {required && <span className="required">*</span>}
+            </label>
+          )}
+        </div>
         {fieldLabel && <div className="my-2.5" />}
-        <Select
+        <input
           id={id}
-          isMulti={isMulti}
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          disabled={disabled}
+          onChange={onChange}
           required={required}
-          isSearchable={true}
-          options={options}
-          onChange={(selectedOption) =>
-            group
-              ? onChange(id, selectedOption ? selectedOption.value : "")
-              : onChange(selectedOption.value)
-          }
-          defaultValue={
-            group ? options.find((option) => option.value === value) : value
-          }
-          isClearable={true}
-          styles={{
-            control: (baseStyles, state) => ({
-              ...baseStyles,
-              height: "40px",
-              padding: "2px",
-              backgroundColor: "#E5E7EB",
-              border: "none",
-              borderRadius: "0",
-            }),
-          }}>
-          <option>select ...</option>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </Select>
+          autoComplete="false"
+          className={`w-full p-2 bg-gray-200  ${
+            (error ? "border border-red-500" : "", disabled ? "bg-gray-100" : "")
+          }`}
+        />
+        {IsMultiInput && (
+          <DynamicInput 
+            label={label}
+            initialValue= {value} 
+          />
+        )}
       </div>
     );
-  };
+  }
