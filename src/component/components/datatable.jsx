c4c74afbd9f0cd7b8e5de9f1e992b6e1e1019bb1
Bobby-gh <knaihv@ymail.com>
2025-02-07 00:39:48 +0000

    working on side bar

--- src/component/components/datatable.jsx
+++ src/component/components/datatable.jsx
@@ -32,10 +32,6 @@ export const useUserColumns = () => {
     },
     {
       accessorKey: "email",
-      header: t("createdAt"),
-    },
-    {
-      accessorKey: "email",
       header: t("email"),
     },
     {
