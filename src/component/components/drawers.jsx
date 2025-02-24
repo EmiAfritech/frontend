b91323b576e69b2b26ebf56d60b8cadcf08a415e
Bobby-gh <knaihv@ymail.com>
2025-02-24 00:55:40 +0000

    reverted the forms

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -791,7 +791,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
   const { departmentList } = useDepartmentDropdown();
   const {riskToBeMitigated} = useRiskToBeMitigated(departmentID)
   const {riskToBeMitigatedInfo} = useRiskToBeMitigatedInfo(riskName)
-  console.log({"riskINfo":  riskToBeMitigatedInfo.riskCategory})
+  console.log({"riskINfo":  riskToBeMitigatedInfo})
   const hostaddress = "http://localhost:5173/risk-mitigation";
   const [open, setOpen] = useState(false);
   const notify = () => {
