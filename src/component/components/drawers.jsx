ef77f7d7f1d24dd9a02837beaa010c35333e4876
Bobby-gh <knaihv@ymail.com>
2025-03-02 09:15:46 +0000

    working on edit tab

--- src/component/components/drawers.jsx
+++ src/component/components/drawers.jsx
@@ -791,8 +791,7 @@ export function RiskMitigationforms({ onFormSubmit }) {
   const { departmentList } = useDepartmentDropdown();
   const {riskToBeMitigated} = useRiskToBeMitigated(departmentID)
   const {riskToBeMitigatedInfo} = useRiskToBeMitigatedInfo(riskName)
-  const { riskCategory, riskID, probability, impact } = riskToBeMitigatedInfo ?? {};
-  console.log({ riskCategory, riskID, probability, impact } )
+  console.log({"riskID": riskToBeMitigatedInfo.riskID})
   console.log({"riskINfo":  riskToBeMitigatedInfo})
   const hostaddress = "http://localhost:5173/risk-mitigation";
   const [open, setOpen] = useState(false);
