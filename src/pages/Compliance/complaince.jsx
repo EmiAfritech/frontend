2ded89589b89b3a1e58006f545d4358d5fb8753a
Bobby-gh <knaihv@ymail.com>
2025-03-03 00:01:43 +0000

    working on edit tab

--- src/pages/Compliance/complaince.jsx
+++ src/pages/Compliance/complaince.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Sidebar } from "../../component/components/sidebar";;
-import "../../component/comstyles/component.css";
+import { Sidebar } from "../../component/components/sidebar";
+import { ComplianceTab } from "../../component/components/tables";
 
 export function Compliance() {
   return (
     <div className="flex flex-col h-screen">
       <Sidebar />
-      <div className="ml-[18%] m-6 ">
-        Hi there i am compliance
+      <div className="ml-[18%] m-6 p-6 card bg-white rounded-lg">
+        <ComplianceTab />
       </div>
     </div>
   );
