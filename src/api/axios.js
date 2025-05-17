acbd7e6342aeb80cd1c98f13904aea8576a0eace
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-05-17 14:14:43 +0000

    setting new route link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "https://risksaasbackend-production.up.railway.app/api/v1",
+  baseURL: "https://risksaasbackend-production-8bb3.up.railway.app/api/v1/",
 });
+ 
