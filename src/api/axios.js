c5a3efb1c4c1cb7b6756a66a9ad135977dcfa3c6
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-06-20 13:33:18 +0000

    working on risk base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
-  baseURL: "https://risksaasbackend-production-8bb3.up.railway.app/api/v1/",
+ // baseURL: "https://risksaasbackend-production-8bb3.up.railway.app/api/v1/",
+ baseURL: "https://ermbackend.afriquetek.com/api/v1/",
 });
  
