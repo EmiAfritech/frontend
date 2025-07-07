d3d27784723b27db8a3d5d56b0e0c030290b9ef2
Bobby-gh <knaihv@ymail.com>
2025-07-07 23:54:23 +0000

    emma working

---
+++ src/component/components/topnavbar.jsx
@@ -0,0 +1,20 @@
+
+import { MdAccountCircle } from "react-icons/md";
+import { CgMenuGridR } from "react-icons/cg";
+
+
+export function TopNavbar() {
+  return (
+    <nav className="flex justify-end">
+      <div className="flex cursor-pointer space-x-4 item-center">
+        {/* <LogoutButton /> */}
+        <div className="flex text-2xl cursor-pointer">
+          <MdAccountCircle />
+        </div>
+        <div className="flex text-2xl cursor-pointer">
+          <CgMenuGridR />
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/pages/dasboard/dashboard.jsx
+++ src/pages/dasboard/dashboard.jsx
@@ -7,12 +7,14 @@ import {
   MonitoredVsUnmonitored,
 } from "../../component/components/charts";
 import "../../component/comstyles/component.css";
+import { TopNavbar } from "../../component/components/topnavbar";
 
 export function Dashboard() {
   return (
     <div className="flex flex-col h-screen">
       <Sidebar />
       <div className="ml-[18%] m-6 ">
+        <div><TopNavbar /></div>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-2 mb-24">
           <OpenVsClose />
           <ReviewedVsUnreviewed />
