99368f8ca5ef67448af9b24153b497cb32c24873
Bobby-gh <knaihv@ymail.com>
2025-07-08 01:04:20 +0000

    emma working

--- src/App.jsx
+++ src/App.jsx
@@ -76,8 +76,7 @@ export default function App() {
           />
           <Route path="chatbot" element={<ReportRiskChatbot />} />
         </Route>
-        <Route path="/risk-ai" element={<RiskAi />}>
-          <Route index element={<HomeAi data-id="kxjqa1ayv"/>} data-id="4bpvzbj8u"/>
+        <Route path="/risk-ai" element={<HomeAi />}>
           <Route path="/features/:feature" element={<FeatureDetail data-id="a4453m1gz"/>} data-id="ihu3vnod0"/>
           <Route path="/analytics" element={<AnalyticsAi data-id="zkaqcxnpm"/>} data-id="6906oed92"/>
           <Route path="/settings" element={<SettingsAi data-id="rsvp6or3h"/>} data-id="vlqsdhrsh"/>
