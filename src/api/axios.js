845086cf027cd7b3f45ca34306fed708a6c17a38
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-08-02 19:06:52 +0000

    working on risk link

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,6 @@ export default axios.create({
  // baseURL: "https://risksaasbackend-production-8bb3.up.railway.app/api/v1/",
 //  baseURL: "https://ermbackend.afriquetek.com/api/v1/",
  //   baseURL: "https://xxp2v4h8-8080.uks1.devtunnels.ms/api/v1"
- baseURL: "https://risksaasbackend-production.up.railway.app/api/v1/"
+ baseURL: "https://risksaasbackend-production.up.railway.app/api/v1"
 });
  
