119b277da2ec74b50b3fb3871161a855847dc039
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-11-10 10:05:31 +0000

    working on routes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 export default axios.create({
   //https://bkdeployments-production.up.railway.app/api
 
-  baseURL: "https://ermbackend.afriquetek.com/api/v1/",
- //  baseURL: "https://xxp2v4h8-8080.uks1.devtunnels.ms/api/v1"
+ // baseURL: "https://ermbackend.afriquetek.com/api/v1/",
+  baseURL: "http://dev-ermbackend.afriquetek.com/"
  //baseURL: "https://risksaasbackend-production.up.railway.app/api/v1"
   //baseURL: "risksaasbackend-production-4eb8.up.railway.app"
 });
